1aac613a3671d59f02c1359551f873bd24aabee7
sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-01-22 21:28:28 +0530

    completed

--- Calculator.java
+++ Calculator.java
@@ -38,7 +38,44 @@ public class Calculator {
             System.out.println("Error: Division by zero is not allowed!");
     }
 
-    
+    //  function for Fibonacci Sequence
+    public static void fibonacci() {
+        System.out.print("Enter the number of Fibonacci terms: ");
+        int n = scanner.nextInt();
+        int a = 0, b = 1;
+        System.out.print("Fibonacci sequence: " + a + " " + b);
+        for (int i = 2; i < n; i++) {
+            int c = a + b;
+            System.out.print(" " + c);
+            a = b;
+            b = c;
+        }
+        System.out.println();
+    }
+
+    //  function to calculate the Mean of Array
+    public static void meanOfArray() {
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+        double[] array = getArrayInput(size);
+        double sum = 0;
+        for (double num : array)
+            sum += num;
+        System.out.println("Mean of array elements: " + (sum / size));
+    }
+
+    //  function to calculate the Variance of Array
+    public static void varianceOfArray() {
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+        double[] array = getArrayInput(size);
+        double mean = calculateMean(array);
+        double variance = 0;
+        for (double num : array)
+            variance += Math.pow(num - mean, 2);
+        System.out.println("Variance of array elements: " + (variance / size));
+    }
+
     // Helper: Input Array
     private static double[] getArrayInput(int size) {
         double[] array = new double[size];
